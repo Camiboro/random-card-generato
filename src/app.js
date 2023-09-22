43299cb01764f9a076849c37cdc28d1ad88f716a
Camiboro <130386618+Camiboro@users.noreply.github.com>
2023-09-22 22:01:45 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,49 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateCard() {
+  const suits = ["spade", "club", "heart", "diamond"];
+  const suitSymbols = {
+    spade: "♠",
+    club: "♣",
+    heart: "♥",
+    diamond: "♦"
+  };
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  const randomSuit = suits[getRandomInt(0, 3)];
+  const randomValue = values[getRandomInt(0, 12)];
+
+  return {
+    suit: randomSuit,
+    value: randomValue,
+    symbol: suitSymbols[randomSuit]
+  };
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardContainer = document.getElementById("cardContainer");
+  const card = generateCard();
+  cardContainer.classList.add(card.suit);
+
+  cardContainer.innerHTML = `
+      <div class="top">${card.value} ${card.symbol}</div>
+      <div class="center">${card.symbol}</div>
+      <div class="bottom">${card.value} ${card.symbol}</div>
+  `;
 };
